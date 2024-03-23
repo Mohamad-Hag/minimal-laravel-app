3bfe99e0162c5ca4604a08d8e5d1e1b328d302b9
Mohamad-Hag <mohamadhag99@gmail.com>
2024-03-24 02:45:49 +0400

    edit is_admin prop

--- database/migrations/2024_03_23_202346_add_is_admin_to_users_table.php
+++ database/migrations/2024_03_23_202346_add_is_admin_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('is_admin')->default('0');
+            $table->string('is_admin')->default('1');
         });
     }
 
